30914adf872d34ec67c1386ca2de23c78c4a7513
TriUnityTech <contato@triunitytech.com.br>
2026-07-08 14:40:55 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
@@ -71,6 +72,18 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.png" type="image/png" sizes="96x96" />
       </head>
       <body className={inter.className}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-5X1FVTE8V9"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5X1FVTE8V9');
+          `}
+        </Script>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
